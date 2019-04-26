219fcbee625dc2f50af9ff33ca93a3f2ad47c750
Alexander Alyokhin <alexander.alyokhin@valtech.com>
2019-04-26 15:41:54 +0300

    Change the way how it renders

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import WordsCloud from './components/WordsCloud.vue';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  el: '#vue-app',
-  components: {
-    WordsCloud,
-  },
-});
+const WordsCloudInstance = Vue.extend(WordsCloud);
+
+new WordsCloudInstance().$mount('#words-cloud');
